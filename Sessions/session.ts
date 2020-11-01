d8a71c631c4903e498db44586e3091ae867b7002
Goose <cpgusev@gmail.com>
2020-11-01 13:51:41 -0500

    Added in edits from PR

--- Sessions/session.ts
+++ Sessions/session.ts
@@ -10,33 +10,50 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.post('/token', urlencodedParser, async (req, res) => {
 
-    const userId = req.body.userId
-    const passId = req.body.passId
 
-    // let response = await axios.post('/login', {
-    //     'userId': userId,
-    //     'passId': passId
-    // })
+    
+    let userId, passId
+    
+    try{
+      userId = req.body.userId
+      passId = req.body.passId
+    }
+    catch{
+      res.statusCode = 400
+      res.send("Bad request: userId or passId malformated")
+    }
 
-    // if(response.status != 200){
-      // res.statusCode = response.status
-      // res.send('Something has gone wrong!')
-    // }
-    // else{
+    let response = await axios.get('accounts:3000/login', 
+    {
+      headers: {
+        'Authorization' : {
+          'username' : userId,
+          'password' : passId
+        }
+      }
+    })
+
+    if(response.status != 200){
+      res.statusCode = response.status
+      res.send(response)
+    }
+    else{
 
 
     const token = jwt.sign({ userId }, process.env.JWT_KEY, {
         algorithm: "HS256",
         expiresIn: process.env.JWT_KEY_EXPIRY,
       })
+
       console.log("token:", token)
     
       // set the cookie as the token string, with a similar max age as the token
       // here, the max age is in milliseconds, so we multiply by 1000
       res.cookie("token", token, { maxAge: parseInt(process.env.JWT_KEY_EXPIRY) * 1000 })
+      res.send(token)
       res.end()
 
-    // }
+    }
 })
 
 app.listen(port, () => {
