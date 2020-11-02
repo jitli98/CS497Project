bc96ef40ea06b87b9ad20da783025b6a0a8478d9
Example <example@example.com>
2020-11-01 20:01:33 -0500

    added challenges service to docker-compose and added env variables for db

--- challenges_service/db.js
+++ challenges_service/db.js
@@ -1,11 +1,9 @@
 // db.js
+require('dotenv').config();
 const mongoose = require("mongoose");
+const url = process.env.DATABASE_URL;
 
-const account = 'ampinto';
-const password = 'n6u7V5wxoPmr6piX';
-const uri = `mongodb+srv://${account}:${password}@cluster0.l4vjr.mongodb.net/challenges?retryWrites=true&w=majority`;
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }).catch(error => console.log("db.js error: " + error));;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }).catch(error => console.log("db.js error: " + error));;
 
 mongoose.connection.on('connected', function(){
     console.log("db.js: Connected to MongoDB");
